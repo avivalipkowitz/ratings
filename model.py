342c00ac5e31432623a852c4b04924b85e0dc363
Hackbright Student <info@hackbrightacademy.com>
2014-06-30 17:52:37 -0700

    Initial final commit for part 1

--- model.py
+++ model.py
@@ -1,13 +1,54 @@
+import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import sessionmaker
+
+ENGINE = None
+Session = None
 
 Base = declarative_base()
 
 ### Class declarations go here
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key = True)
+    email = Column(String(64), nullable = True)
+    password = Column(String(64), nullable = True)
+    age = Column(Integer, nullable = True)
+    zipcode = Column(String(15), nullable = True)
+
+class Movie(Base):
+    __tablename__ = "movies"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(64), nullable = False)
+    release_date = Column(DateTime, nullable = True)
+    imdb_url = Column(String(64), nullable = True)
+
+class Data(Base):
+    __tablename__ = "data"
+
+    id = Column(Integer, primary_key = True, autoincrement = True)
+    user_id = Column(Integer, nullable = False)
+    movie_id = Column(Integer, nullable = False)
+    rating = Column(Integer, nullable = False)
+
 
 
 ### End class declarations
+
+def connect():
+    global ENGINE
+    global Session
+
+    ENGINE = create_engine("sqlite:///ratings.db", echo = False)
+    Session = sessionmaker(bind=ENGINE)
+
+    return Session()
+
+
 
 def main():
     """In case we need this for something"""
--- seed.py
+++ seed.py
@@ -2,21 +2,63 @@ import model
 import csv
 
 def load_users(session):
-    # use u.user
-    pass
+    input_file = open("seed_data/u.user")
+
+    for line in input_file:
+        line = line.rstrip()
+        entry = line.split("|")
+        id = int(entry[0])
+        age = int(entry[1])
+        gender = str(entry[2]) #unused because we are gender neutral 
+        occupation = str(entry[3]) #unused on purpose
+        zipcode = str(entry[4])
+
+        user = model.User(id = id, age = age, zipcode = zipcode)
+        session.add(user)
+    session.commit()
+
 
 def load_movies(session):
-    # use u.item
-    pass
+    input_file = open("seed_data/u.item")
+
+    for line in input_file:
+        line = line.rstrip()
+        entry = line.split("|")
+        id = int(entry[0])
+        title = entry[1].split('(') #splitting date in parens off of title
+        title = str(title[0]).decode("latin-1")
+        release_date = entry[2]
+        if not release_date:
+            release_date = None
+        else:
+            release_date = model.datetime.datetime.strptime(release_date, '%d-%b-%Y')
+        imdb_url = str(entry[4])
+
+        movie = model.Movie(id = id, title = title, release_date = release_date, 
+            imdb_url = imdb_url)
+        session.add(movie)
+    session.commit()
 
 def load_ratings(session):
-    # use u.data
-    pass
+    input_file = open("seed_data/u.data")
+    
+    for line in input_file:
+        line = line.rstrip()
+        entry = line.split()
+        user_id = int(entry[0])
+        movie_id = int(entry[1])
+        rating = int(entry[2])
+
+        data = model.Data(user_id=user_id, movie_id=movie_id, rating=rating)
+        session.add(data)
+    session.commit()
 
 def main(session):
-    # You'll call each of the load_* functions with the session as an argument
-    pass
+    #load_users(session)
+    load_movies(session)
+    #load_ratings(session)
 
+    
 if __name__ == "__main__":
     s= model.connect()
     main(s)
